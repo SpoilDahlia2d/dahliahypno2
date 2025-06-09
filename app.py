e0d1686edf7c358cc88979bee50b7b228ad60dbf
SpoilDahlia2d <dahliastar1998@gmail.com>
2025-06-09 22:45:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template, send_from_directory, jsonify
 import os
+import random
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/images")
-def get_images():
-    folder = os.path.join(app.static_folder, "images")
-    files = [f for f in os.listdir(folder) if f.lower().endswith(".png")]
-    return jsonify(files)
+@app.route('/images')
+def images():
+    path = os.path.join(app.static_folder, 'images')
+    all_files = os.listdir(path)
+    imgs = [f for f in all_files if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
+    return jsonify(imgs)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
